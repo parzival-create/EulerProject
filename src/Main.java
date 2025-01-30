bd96b1c8643ea747ed40108246e55ba8b03b65b4
parzival-create <dariusa.hoffmann@t-online.de>
2025-01-30 15:09:29 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ public class Main {
         int sum = 0;
 
         for (int i = 0; i < 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0)
-            {
+            if (i % 3 == 0 || i % 5 == 0) {
                 sum += i;
             }
         }
